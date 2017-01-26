0feb05bf5f4fa5218a167280714f06fc16cac7e7
hezi-bd <hezi.bd@gmail.com>
2017-01-27 01:56:23 +0200

    nav test

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,17 +1,17 @@
 $(function() {
   designBGStuff();
   smoothScroll(300);
-  mobileNav();
+  // mobileNav();
 });
 
 
-function mobileNav() {
-  $('.mobile-nav-toggle').on('click', function(){
-    var status = $(this).hasClass('is-open');
-    if(status){ $('.mobile-nav-toggle, .mobile-nav').removeClass('is-open'); }
-    else { $('.mobile-nav-toggle, .mobile-nav').addClass('is-open'); }
-  });
-}
+// function mobileNav() {
+//   $('.mobile-nav-toggle').on('click', function(){
+//     var status = $(this).hasClass('is-open');
+//     if(status){ $('.mobile-nav-toggle, .mobile-nav').removeClass('is-open'); }
+//     else { $('.mobile-nav-toggle, .mobile-nav').addClass('is-open'); }
+//   });
+// }
 
 
 function smoothScroll (duration) {
